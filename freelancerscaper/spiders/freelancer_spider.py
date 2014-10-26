ee6ee734deceefbb46500ae03de6ab46be4bdca3
adi <adi@kleptovici>
2014-10-26 13:38:42 +0200

    added tabulate for pretty printing of data

--- freelancerscaper/spiders/freelancer_spider.py
+++ freelancerscaper/spiders/freelancer_spider.py
@@ -5,6 +5,7 @@
 from scrapy.selector import HtmlXPathSelector
 from scrapy.spider import BaseSpider
 from freelancerscraper.items import FreelancerscraperItem
+from tabulate import tabulate
 import operator
 import re
 class FreelancerSpider(BaseSpider):
@@ -12,21 +13,20 @@ class FreelancerSpider(BaseSpider):
 	start_urls = [ "https://www.freelancer.com/job/"  ]
 
 	def parse(self, response):
-		filename = response.url.split("/")[-2]
 		htmlresponseinit = HtmlXPathSelector(text=response.body).select('//div[@id="browseJobs"]')
 		i_range = range(13)
 		for sel in htmlresponseinit:
 			items = []
 			for i in i_range:
-				
+
 			        item = FreelancerscraperItem()
 				id_a_link = "jobCat" + str(i) + "_heading"
-			        id_ul_link = "jobCat" + str(i)	
+			        id_ul_link = "jobCat" + str(i)
 			        parent_categ_name = sel.select('p/strong/a[@id="' + id_a_link + '"]/text()').extract()
 				item['parent_categ_name'] = parent_categ_name
-			        categ_and_count_string =	(sel.select('ul[@id="' + id_ul_link  + '"]/li/a/text()').extract())	
+			        categ_and_count_string =	(sel.select('ul[@id="' + id_ul_link  + '"]/li/a/text()').extract())
 
-			        result = {} 
+			        result = {}
 				for categ_and_count in categ_and_count_string:
 			           #   Electronics (1234)
 				   categ = categ_and_count.split("(")[0].strip()
@@ -35,8 +35,10 @@ class FreelancerSpider(BaseSpider):
 				sorted_result = sorted(result.items(), key=operator.itemgetter(1), reverse = True)
 				item['categ_name_count'] = sorted_result
 			        items.append(item)
-	          	print items
+                for item in items:
+                        print "--------------*******------------------------------*******----------------"
+                        print tabulate(item['categ_name_count'], ["CATEGORY: " + "".join(item['parent_categ_name']), "Count"], tablefmt="grid")
+                                
+		yield None
 
-			yield None
 
-		
